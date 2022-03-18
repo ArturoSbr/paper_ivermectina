9af51dbbd9b6e4fec21941b83455c8736d19112e
ArturoSbr <soberonarturo@gmail.com>
2022-03-18 14:25:11 -0600

    Removed redundant files

--- 01_code/descarga_datos.R
+++
@@ -1,4 +0,0 @@
-pacman::p_load(googlesheets4)
-
-read_sheet("https://docs.google.com/spreadsheets/d/1VtXKW1IuCm4qRowlotXnTWZlhLoQYYmEsZp7ERUIeAQ") %>%
-write_rds(here::here("02_out", "base_publica.rds"))
